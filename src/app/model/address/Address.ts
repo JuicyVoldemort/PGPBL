a763598cd80f58686ce7ffac3f314b6202f206ec
JuicyVoldemort <calvinrosmuhammadramadhino@mail.ugm.ac.id>
2024-09-08 16:37:32 +0700

    State, Action, Reducers build. Register Page

---
+++ src/app/model/address/Address.ts
@@ -0,0 +1,9 @@
+export class Address {
+    address!: string;
+    number!: string;
+    neighborhood!: string;
+    complement!: string;
+    zipcode!: string;
+    state!: string;
+    city!: string;
+}
--- src/store/AppStoreModule.ts
+++ src/store/AppStoreModule.ts
@@ -4,12 +4,18 @@ import {loginReducer} from "src/store/login/login.reducers";
 import { AppState } from 'src/store/AppState';
 import { EffectsModule } from "@ngrx/effects";
 import { LoginEffects } from "src/app/pages/login/login.effects";
+import { registerReducer } from "./register/register.reducer";
 
 export const AppStoreModule = [
     StoreModule.forRoot([]),
     StoreModule.forFeature("loading", loadingReducer),
     StoreModule.forFeature("login", loginReducer),
+    StoreModule.forFeature("register", registerReducer),
     EffectsModule.forRoot([
         LoginEffects
     ])
 ]
+
+
+// Vid nomor playlist 24 membangun 
+// State, Actions, Reducers untuk halaman register. Prosedur hampir sama untuk Login page
