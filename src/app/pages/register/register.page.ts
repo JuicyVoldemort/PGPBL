4b4aa21dfa82767df4a6a0d3b7d3c5185f63e477
JuicyVoldemort <calvinrosmuhammadramadhino@mail.ugm.ac.id>
2024-09-09 00:41:48 +0700

    Implementasi alur user register

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router'; // Import Router
 import { RegisterPageForm } from './form/register.page.form';
 import { FormBuilder } from '@angular/forms';
+import { register } from 'src/store/register/register.action';
+import { AppState } from 'src/store/AppState';
+import { Store } from '@ngrx/store';
+import { RegisterState } from 'src/store/register/RegisterState';
+import { hide, show } from 'src/store/loading/loading.actions';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-register',
@@ -12,23 +18,50 @@ export class RegisterPage implements OnInit {
 
   registerForm!: RegisterPageForm;
 
-  constructor(private router: Router, private formBuilder:FormBuilder) {}
-
+  constructor(private router: Router, private formBuilder: FormBuilder, private store: Store<AppState>, 
+    private toastController: ToastController){ }
 
   ngOnInit() {
     this.createForm();
+
+    this.watchRegisterState();
   }
 
   register() {
     this.registerForm.getForm().markAllAsTouched();
 
-    if (this.registerForm.getForm().valid){
-    this.router.navigate(['home']);
+    if (this.registerForm.getForm().valid) {
+      this.store.dispatch(register({userRegister: this.registerForm.getForm().value}));
+    }
   }
-}
 
   private createForm() {
     this.registerForm = new RegisterPageForm(this.formBuilder);
   }
 
+  private watchRegisterState() {
+    this.store.select('register').subscribe(state => {
+      this.toggleLoading(state);
+
+      if(state.isRegistered){
+        this.router.navigate(['home']);
+      }
+
+      if(state.error){
+        this.toastController.create({
+           message: state.error.message,
+           duration: 5000,
+           header: 'Registraion not done'
+        }).then(toast => toast.present());
+      }
+    })
+  }
+
+  private toggleLoading(state: RegisterState) {
+    if (state.isRegistering){
+      this.store.dispatch(show());
+    } else {
+      this.store.dispatch(hide());
+    }
+  }
 }
